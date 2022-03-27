ce0808ee67ae1f6b5786890a54e6185abaff0b05
Henrique <up201805000@up.pt>
2022-03-27 18:15:03 +0100

    boards done

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+from draw import draw_big_board, draw_small_board
 from piece import *
+import utils
 import pygame
 import pygame_menu 
 
@@ -37,10 +39,13 @@ def remove_moves(piece):
 
 def main():
     
-    board_num=input("Select board(1-20):")
+    playing =True
+    pygame.init()
 
+    screen = pygame.display.set_mode(utils.GAME_WINDOW_SIZE)
+    pygame.display.set_caption('Chess Snake')
     print_board(board)
-
+    print(utils.BOARD_SMALL)
     rook = Rook((2, 3))
     bishop = Bishop((3, 1))
     king = King (1,1)
@@ -48,14 +53,30 @@ def main():
     knight = Knight(0,4)
     pieces = [rook, bishop, king, queen, knight]
 
-    for piece in pieces:
-        if type(piece).__name__ != 'King' and type(piece).__name__ != 'Knight':
-            for i in range(1, 5):
-                piece.set_moves(i)
-        else:
-            piece.set_moves()
-        remove_moves(piece)
-        print(type(piece).__name__, piece.moves)
+    screen.fill(utils.GAME_WINDOW_COLOR)
+    ##6x6
+    #draw_big_board(screen)
+    ##5x5
+    draw_small_board(screen)
+    pygame.display.update()
+
+
+    while(playing):
+
+   
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+             playing = False
+                   
+
+   ##for piece in pieces:
+    #    if type(piece).__name__ != 'King' and type(piece).__name__ != 'Knight':
+    #        for i in range(1, 5):
+    #            piece.set_moves(i)
+     #    else:
+      #      piece.set_moves()
+       # remove_moves(piece)
+        #print(type(piece).__name__, piece.moves) 
         
 
 if __name__ == "__main__":
