515c4e51dcedbb1226adbd0c9a4697f208e44169
Henrique <up201805000@up.pt>
2022-03-29 16:33:53 +0100

    board working until lvl10

--- src/draw.py
+++ src/draw.py
@@ -5,6 +5,9 @@ WHITE = (255, 255, 255)
 BOARD_BIG=[(1,1),(720,1),(0,0),(0,720),(0,720),(720,720),(720,720),(720,0)]
 BOARD_SMALL=[(1,1),(720,1),(0,0),(0,720),(0,720),(720,720),(720,720),(720,0)]
 
+def get_font(size):
+    return pygame.font.Font("assets/font2.ttf", size)
+
 for i in range(120,720,120):
     BOARD_BIG.append((0,i))
     BOARD_BIG.append((720,i))
@@ -16,10 +19,24 @@ for i in range(144,720,144):
 def draw_big_board(screen):
     draw_lines(screen, BOARD_BIG)
     draw_cols(screen, BOARD_BIG)
+    S_TEXT = get_font(100).render("S", True, "Black")
+    S_RECT = S_TEXT.get_rect(center=(60, 660))
+    screen.blit(S_TEXT, S_RECT)
+    F_TEXT = get_font(100).render("F", True, "Black")
+    F_RECT = F_TEXT.get_rect(center=(660, 60))
+    screen.blit(F_TEXT, F_RECT)
+
 
 def draw_small_board(screen):
     draw_lines(screen, BOARD_SMALL)
     draw_cols(screen, BOARD_SMALL)
+    S_TEXT = get_font(120).render("S", True, "Black")
+    S_RECT = S_TEXT.get_rect(center=(72, 648))
+    screen.blit(S_TEXT, S_RECT)
+    F_TEXT = get_font(120).render("F", True, "Black")
+    F_RECT = F_TEXT.get_rect(center=(648, 72))
+    screen.blit(F_TEXT, F_RECT)
+
 
 
 def draw_lines(screen,board):
