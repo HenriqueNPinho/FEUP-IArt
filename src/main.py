f60902b0bc03e65af8b0a8eed1f5331a0959ecdf
luisgmarques <ei11159@fe.up.pt>
2022-04-12 23:50:26 +0100

    added stats metrics

--- src/main.py
+++ src/main.py
@@ -51,17 +51,29 @@ def get_new_state(state, dir):
 def bfs(current_state):
     states = Queue()
     states.put(current_state)
+    state_expanded = False
+    states_expanded = 0
+    max_states = 0
     bfs_start = time()
     while not end_state(current_state):
         current_state = states.get()
         for dir in 'LRUD':
             new_state = get_new_state(current_state, dir)
             if valid_state(new_state):
+                max_states += 1
+                state_expanded = True
                 states.put(new_state)
+        print(max_states)
+        input()
+        if state_expanded:
+            states_expanded += 1
+            state_expanded = False
     bfs_end = time()
     
     draw_board(current_state)
-    print(f'BFS Time: {bfs_end - bfs_start}')
+    print(f'BFS Time: {bfs_end - bfs_start} s')
+    print(f'Nodes expanded: {states_expanded}')
+    print(f'Max Memory: {max_states*sys.getsizeof(current_state)/1000} KB')
     print('States: ', states.qsize(), 'Depth: ', current_state.depth, 'Solution: ',current_state.get_path(), sep='\n')
     return current_state
 
@@ -90,6 +102,9 @@ def bfs_test(current_state):
 
 def dfs(current_state):
     states = [current_state]
+    state_expanded = False
+    states_expanded = 0
+    max_states = 0
 
     dfs_start = time()
     while not end_state(current_state):
@@ -98,10 +113,17 @@ def dfs(current_state):
             new_state = get_new_state(current_state, dir)
             if valid_state(new_state):
                 states.append(new_state)
+                max_states += 1
+                state_expanded = True
+        if state_expanded:
+            states_expanded += 1
+            state_expanded = False
     dfs_end = time()
     
     draw_board(current_state)
-    print(f'DFS Time: {dfs_end - dfs_start}')
+    print(f'DFS Time: {dfs_end - dfs_start} s')
+    print(f'Nodes expanded: {states_expanded}')
+    print(f'Max Memory: {max_states*sys.getsizeof(current_state)/1000} KB')
     print('Solution: ', current_state.get_path(), sep='\n')
     return current_state
 
